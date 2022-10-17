21f77d8c72ddf05a136396da08f94134ee46e28d
jeff jeff jeff <jeffernmalinao82@gmail.com>
2022-10-17 19:08:02 +0800

    strawhats

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+const App = () => {
+  return (
+    <div className="container">
+      <div className="title">
+        <h1>strawhats</h1>
+        <h2>strawhats</h2>
+        <h3>strawhats</h3>
+        <h4>strawhats</h4>
+        <h5>strawhats</h5>
+        <h6>strawhats</h6>
+        <p>strawhats</p>
+      </div>
+      <div className="signature">
+        <h1>jeffern</h1>
+      </div>
+    </div>
+  );
+};
+
+export default App;
